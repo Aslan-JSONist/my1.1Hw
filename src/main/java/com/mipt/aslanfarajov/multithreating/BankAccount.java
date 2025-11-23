3e1e8f7f27f5c1350d0c4466a8b555fdcb439ba7
Aslan Farajov <faradzhov.akh@phystech.edu>
2025-11-24 01:10:37 +0300

    12 week HomeWork is ready and multithreading is done

---
+++ src/main/java/com/mipt/aslanfarajov/multithreating/BankAccount.java
@@ -0,0 +1,27 @@
+package com.mipt.aslanfarajov.multithreating;
+
+public class BankAccount {
+    private final int id;
+    private int balance;
+
+    public BankAccount(int id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+}
